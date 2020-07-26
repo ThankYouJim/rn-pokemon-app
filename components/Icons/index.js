924b4f4ade44de64290e58a93f2cd899f7976623
Jo Chong <jo.chong@mav3rik.com>
2020-07-26 17:28:28 +1000

    rename

---
+++ components/Icons/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import bug from '../resource/type/bug.svg';
+import dark from '../resource/type/dark.svg';
+import dragon from '../resource/type/dragon.svg';
+import electric from '../resource/type/electric.svg';
+import fairy from '../resource/type/fairy.svg';
+import fighting from '../resource/type/fighting.svg';
+import fire from '../resource/type/fire.svg';
+import flying from '../resource/type/flying.svg';
+import ghost from '../resource/type/ghost.svg';
+import grass from '../resource/type/grass.svg';
+import ground from '../resource/type/ground.svg';
+import ice from '../resource/type/ice.svg';
+import normal from '../resource/type/normal.svg';
+import poison from '../resource/type/poison.svg';
+import psychic from '../resource/type/psychic.svg';
+import rock from '../resource/type/rock.svg';
+import steel from '../resource/type/steel.svg';
+import water from '../resource/type/water.svg';
+
+export const Grass = <grass fill="green" />;
